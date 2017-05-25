f60874e3536767541d31ad4965c4dc510395d2b3
igorrvasconcelos <igorrvasconcelos00@gmail.com>
2017-05-25 17:22:58 -0300

    contagemvogais

--- cotagem vogais/cotagemvogais.cs
+++ cotagem vogais/cotagemvogais.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            char pos;
+            int i, vogal;
+
+            vogal = 0;
+
+            Console.WriteLine("informe o texto desejado");
+            s = Console.ReadLine();
+
+            for (i = 0; i < s.Length; i++)
+            {
+                pos = s[i];
+
+                if (pos == 'a' || pos == 'A' || pos == 'e' || pos == 'E' || pos == 'i' || pos == 'I' || pos == 'o' || pos == 'O' || pos == 'u' || pos == 'U')
+                    vogal++;
+
+            }
+
+            Console.WriteLine("Número de vogais: {0} vogais", vogal);
+        }
+    }
+}
